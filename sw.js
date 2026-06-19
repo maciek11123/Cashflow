669e59c2b91d8925e347c1aac9adfac02ee94652
maciek11123 <maciekwilkos@gmail.com>
2026-06-19 08:57:45 -0400

    fix: SW scope /Cashflow/

--- sw.js
+++ sw.js
@@ -1,14 +1,24 @@
-const CACHE='cashflow-v1';
+const CACHE='cashflow-v3';
 self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','index.html','/manifest.json','/icon-192.png','/icon-512.png'])));
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll([
+      '/Cashflow/',
+      '/Cashflow/index.html',
+      '/Cashflow/manifest.json',
+      '/Cashflow/icon-192.png',
+      '/Cashflow/icon-512.png'
+    ]))
+  );
   self.skipWaiting();
 });
 self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+  );
 });
 self.addEventListener('fetch',e=>{
   const url=new URL(e.request.url);
-  if(url.pathname==='/'||url.pathname.endsWith('index.html')){
+  if(url.pathname.endsWith('index.html')||url.pathname==='/Cashflow/'){
     e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
     return;
   }
